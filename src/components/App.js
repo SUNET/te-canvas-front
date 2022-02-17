3d3c9a851226db6a54f91f0730b9e6edfa61512a
Ernst Widerberg <ernst@sunet.se>
2022-02-17 12:47:08 +0100

    Squash dev branch

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,280 @@
 import React from "react";
 
-import { Button, InstUISettingsProvider, canvas } from "@instructure/ui";
+import {
+    Button,
+    Heading,
+    InstUISettingsProvider,
+    SimpleSelect,
+    View,
+    canvas
+} from "@instructure/ui";
+
+import { IconTrashLine, IconPlusLine } from "@instructure/ui-icons";
+
+import AsyncSelect from "./AsyncSelect";
 
 import "../style.css";
 
+let MyContext = React.createContext();
+
+function parseResponse(fetchPromise, jsonCallback) {
+    fetchPromise
+        .then(resp => {
+            if (resp.status !== 200)
+                throw new Error(
+                    `Unexpected HTTP response from backend: ${resp.status} ${resp.statusText}`
+                );
+            return resp.json();
+        })
+        .then(jsonCallback)
+        .catch(e => console.error(e));
+}
+
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchObjects: {}
+        };
+        this.refresh = this.refresh.bind(this);
+    }
+
+    componentDidMount() {
+        this.refresh();
+    }
+
+    refresh() {
+        // Arrow function callbacks mandatory for binding of `this`
+        let promise = fetch(process.env.TE_CANVAS_URL + "/api/connection");
+        parseResponse(promise, json => {
+            json.data
+                .filter(x => !x.delete_flag)
+                .forEach(x => {
+                    let promise = fetch(
+                        process.env.TE_CANVAS_URL +
+                            `/api/timeedit/object?extid=${x.te_group}`
+                    );
+                    parseResponse(promise, json => {
+                        this.setState(prevState => ({
+                            searchObjects: {
+                                ...prevState.searchObjects,
+                                [json.data.extid]: {
+                                    extid: json.data.extid,
+                                    type: json.data["type.name"],
+                                    id: json.data["general.id"],
+                                    title: json.data["general.title"]
+                                }
+                            }
+                        }));
+                    });
+                });
+        });
+    }
+
     render() {
         return (
-            <InstUISettingsProvider theme={canvas}>
-                <Button>Hello from InstUI!</Button>
-            </InstUISettingsProvider>
+            <MyContext.Provider value={{ refresh: this.refresh }}>
+                <InstUISettingsProvider theme={canvas}>
+                    <div id="main">
+                        <SearchObjects
+                            refresh={this.refresh}
+                            searchObjects={this.state.searchObjects}
+                        />
+                        <AddNew />
+                    </div>
+                </InstUISettingsProvider>
+            </MyContext.Provider>
+        );
+    }
+}
+
+class SearchObjects extends React.Component {
+    render() {
+        return (
+            <>
+                <Heading border="bottom">TimeEdit Sync</Heading>
+                {Object.entries(this.props.searchObjects).map(([k, v]) => (
+                    <SearchObject key={k} {...v} />
+                ))}
+            </>
+        );
+    }
+}
+
+class SearchObject extends React.Component {
+    constructor(props) {
+        super(props);
+        this.delete = this.delete.bind(this);
+    }
+
+    static contextType = MyContext; // Telling React that I want to use the context provider I have defined
+
+    delete(id) {
+        let promise = fetch(
+            process.env.TE_CANVAS_URL +
+                `/api/connection?te_group=${id}&canvas_group=EXAMPLE`,
+            {
+                method: "DELETE"
+            }
+        );
+        parseResponse(promise, json => {
+            this.context.refresh();
+        });
+    }
+
+    render() {
+        return (
+            <View
+                display="block"
+                borderRadius="medium"
+                borderWidth="small"
+                background="secondary"
+                padding="medium"
+                margin="small"
+            >
+                <div
+                    className="search-object"
+                    style={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        alignItems: "center"
+                    }}
+                >
+                    <div>{this.props.type}</div>
+                    <div>{this.props.title}</div>
+                    <div>{this.props.id}</div>
+                    <Button
+                        renderIcon={IconTrashLine}
+                        onClick={() => this.delete(this.props.id)}
+                    >
+                        Remove
+                    </Button>
+                </div>
+            </View>
+        );
+    }
+}
+
+class AddNew extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            active: false
+        };
+        this.setActive = this.setActive.bind(this);
+    }
+
+    setActive(b) {
+        this.setState({
+            active: b
+        });
+    }
+
+    render() {
+        return this.state.active ? (
+            <AddNewForm setActive={this.setActive} />
+        ) : (
+            <Button
+                renderIcon={IconPlusLine}
+                margin="small"
+                onClick={() => this.setActive(true)}
+            >
+                Add Object
+            </Button>
+        );
+    }
+}
+
+class AddNewForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            type: null,
+            types: [],
+            object: null
+        };
+        this.handleSelect = this.handleSelect.bind(this);
+        this.submit = this.submit.bind(this);
+        this.setObject = this.setObject.bind(this);
+    }
+
+    static contextType = MyContext;
+
+    componentDidMount() {
+        let promise = fetch(process.env.TE_CANVAS_URL + "/api/timeedit/types");
+        parseResponse(promise, json => {
+            console.log(json);
+            this.setState({
+                types: Object.entries(json.data).map(([k, v]) => ({
+                    extid: k,
+                    title: v
+                })),
+                type: Object.keys(json.data).some(x => x === "courseevt")
+                    ? "courseevt"
+                    : Object.keys(json.data)[0]
+            });
+        });
+    }
+
+    handleSelect(key) {
+        return (_, { value }) => {
+            this.setState({ [key]: value });
+        };
+    }
+
+    submit() {
+        let promise = fetch(
+            process.env.TE_CANVAS_URL +
+                `/api/connection?te_group=${this.state.object}&canvas_group=EXAMPLE`,
+            {
+                method: "POST"
+            } // TODO: Does not work with "Content-Type" header added (CORS)
+        );
+        parseResponse(promise, json => {
+            console.log(json); // TODO: Should we give feedback?
+            this.props.setActive(false);
+            this.context.refresh();
+        });
+    }
+
+    setObject(object) {
+        this.setState({
+            object: object
+        });
+    }
+
+    render() {
+        return (
+            <View
+                display="block"
+                borderRadius="medium"
+                borderWidth="small"
+                background="secondary"
+                padding="medium"
+                margin="small"
+            >
+                <SimpleSelect
+                    renderLabel="Object Type"
+                    value={this.state.type}
+                    onChange={this.handleSelect("type")}
+                >
+                    {this.state.types.map(t => (
+                        <SimpleSelect.Option key={t.extid} id={t.extid} value={t.extid}>
+                            {t.title}
+                        </SimpleSelect.Option>
+                    ))}
+                </SimpleSelect>
+                <br />
+                <AsyncSelect
+                    type={this.state.type}
+                    setObject={this.setObject}
+                />
+                <br />
+                <Button onClick={this.submit}>Submit</Button>{" "}
+                <Button onClick={() => this.props.setActive(false)}>
+                    Cancel
+                </Button>
+            </View>
         );
     }
 }
---
+++ src/components/AsyncSelect.js
@@ -0,0 +1,212 @@
+import React from "react";
+
+import { Select, Spinner } from "@instructure/ui";
+
+class AsyncSelect extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputValue: "",
+            isShowingOptions: false,
+            isLoading: false,
+            highlightedOptionId: null,
+            selectedOptionId: null,
+            selectedOptionLabel: "",
+            options: []
+        };
+
+        this.refresh = this.refresh.bind(this);
+        this.getOptionById = this.getOptionById.bind(this);
+        this.matchValue = this.matchValue.bind(this);
+        this.handleShowOptions = this.handleShowOptions.bind(this);
+        this.handleHideOptions = this.handleHideOptions.bind(this);
+        this.handleBlur = this.handleBlur.bind(this);
+        this.handleHighlightOption = this.handleHighlightOption.bind(this);
+        this.handleSelectOption = this.handleSelectOption.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    componentDidUpdate(prevProps) {
+        if (this.props.type !== prevProps.type) this.refresh(null);
+    }
+
+    refresh(search_string) {
+        let url =
+            process.env.TE_CANVAS_URL +
+            `/api/timeedit/objects?type=${this.props.type}&number_of_objects=10`;
+
+        if (search_string !== null) url += `&search_string=${search_string}`;
+
+        fetch(url)
+            .then(resp => {
+                if (resp.status !== 200)
+                    throw new Error(
+                        `Unexpected HTTP response from backend: ${resp.status} ${resp.statusText}`
+                    );
+                return resp.json();
+            })
+            .then(json => {
+                this.setState({
+                    options: json.data.map(x => ({
+                        id: x.extid,
+                        label: x["general.id"]
+                    })),
+                    isLoading: false
+                });
+            })
+            .catch(e => console.log(e));
+    }
+
+    getOptionById(queryId) {
+        return this.state.options.find(({ id }) => id === queryId);
+    }
+
+    matchValue() {
+        const { options, inputValue, selectedOptionId, selectedOptionLabel } =
+            this.state;
+
+        // an option matching user input exists
+        if (options.length === 1) {
+            const onlyOption = options[0];
+            // automatically select the matching option
+            if (onlyOption.label.toLowerCase() === inputValue.toLowerCase()) {
+                return {
+                    inputValue: onlyOption.label,
+                    selectedOptionId: onlyOption.id
+                };
+            }
+        }
+        // allow user to return to empty input and no selection
+        if (inputValue.length === 0) {
+            return { selectedOptionId: null };
+        }
+        // no match found, return selected option label to input
+        if (selectedOptionId) {
+            return { inputValue: selectedOptionLabel };
+        }
+    }
+
+    handleShowOptions() {
+        this.setState({
+            isShowingOptions: true
+        });
+    }
+
+    handleHideOptions() {
+        this.setState({
+            isShowingOptions: false,
+            highlightedOptionId: null,
+            ...this.matchValue()
+        });
+    }
+
+    handleBlur() {
+        this.setState({ highlightedOptionId: null });
+    }
+
+    handleHighlightOption(event, { id }) {
+        event.persist();
+        const option = this.getOptionById(id);
+        if (!option) return; // prevent highlighting of empty option
+        this.setState(state => ({
+            highlightedOptionId: id,
+            inputValue:
+                event.type === "keydown" ? option.label : state.inputValue
+        }));
+    }
+
+    handleSelectOption(_, { id }) {
+        const option = this.getOptionById(id);
+        if (!option) return; // prevent selecting of empty option
+        this.setState({
+            selectedOptionId: id,
+            selectedOptionLabel: option.label,
+            inputValue: option.label,
+            isShowingOptions: false,
+            options: [this.getOptionById(id)]
+        });
+
+        // TODO: Let selected object be prop only, not duplicated in child state
+        this.props.setObject(id);
+    }
+
+    handleInputChange(event) {
+        const value = event.target.value;
+
+        if (!value || value === "") {
+            this.setState({
+                isLoading: true,
+                inputValue: value,
+                isShowingOptions: true,
+                selectedOptionId: null,
+                selectedOptionLabel: null,
+                options: []
+            });
+            this.refresh(null);
+        } else {
+            this.setState({
+                isLoading: true,
+                inputValue: value,
+                isShowingOptions: true,
+                options: [],
+                highlightedOptionId: null
+            });
+            this.refresh(value);
+        }
+    }
+
+    render() {
+        const {
+            inputValue,
+            isShowingOptions,
+            isLoading,
+            highlightedOptionId,
+            selectedOptionId,
+            options
+        } = this.state;
+
+        return (
+            <div>
+                <Select
+                    renderLabel="Object ID"
+                    assistiveText="Type to search"
+                    inputValue={inputValue}
+                    isShowingOptions={isShowingOptions}
+                    onBlur={this.handleBlur}
+                    onInputChange={this.handleInputChange}
+                    onRequestShowOptions={this.handleShowOptions}
+                    onRequestHideOptions={this.handleHideOptions}
+                    onRequestHighlightOption={this.handleHighlightOption}
+                    onRequestSelectOption={this.handleSelectOption}
+                >
+                    {options.length > 0 ? (
+                        options.map(option => {
+                            return (
+                                <Select.Option
+                                    id={option.id}
+                                    key={option.id}
+                                    isHighlighted={
+                                        option.id === highlightedOptionId
+                                    }
+                                    isSelected={option.id === selectedOptionId}
+                                >
+                                    {option.label}
+                                </Select.Option>
+                            );
+                        })
+                    ) : (
+                        <Select.Option id="empty-option" key="empty-option">
+                            {isLoading ? (
+                                <Spinner renderTitle="Loading" size="x-small" />
+                            ) : (
+                                "No results"
+                            )}
+                        </Select.Option>
+                    )}
+                </Select>
+            </div>
+        );
+    }
+}
+
+export default AsyncSelect;
