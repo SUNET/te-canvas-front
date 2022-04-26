1871ec420d9ae215881e3cd1ab75aa3e7fe87c28
Ernst Widerberg <ernst@sunet.se>
2022-04-26 16:02:08 +0200

    Make conditional more specific

--- src/util.js
+++ src/util.js
@@ -24,7 +24,7 @@ export function getLtik() {
 
 export function urlParams(baseUrl, path, params) {
     let url = new URL(path, baseUrl);
-    if (!window.injectedEnv.NO_LTI) params.ltik = getLtik();
+    if (window.injectedEnv.NO_LTI !== "1") params.ltik = getLtik();
     url.search = new URLSearchParams(params);
     return url;
 }
