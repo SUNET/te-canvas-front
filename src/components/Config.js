7a04f09b9eb148a372532e107f035ead9b28c1da
Pontus Fagerström <pontus.fagerstrom@fripost.org>
2023-04-20 15:57:59 +0200

    add types selector to config

--- src/components/Config.js
+++ src/components/Config.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-import { Button, Grid, GridCol, Heading, IconPlusLine, IconTrashLine, SimpleSelect, Tag, TextArea, TextInput, View } from "@instructure/ui";
+import { Button, Grid, GridCol, Heading, IconPlusLine, IconTrashLine, Modal, SimpleSelect, Tag, TextArea, TextInput, View } from "@instructure/ui";
 
-import { urlParams } from "../util";
+import { parseResponse, urlParams } from "../util";
 
 class Config extends React.Component {
     constructor(props) {
@@ -52,17 +52,17 @@ class Config extends React.Component {
         return (
             <div id="config">
                 <ConfigSection
-                    name="Title"
+                    name="title"
                     children={this.state.title}
                     onDelete={this.handleDelete}
                 />
                 <ConfigSection
-                    name="Location"
+                    name="location"
                     children={this.state.location}
                     onDelete={this.handleDelete}
                     />
                 <ConfigSection
-                    name="Description"
+                    name="description"
                     children={this.state.description}
                     onDelete={this.handleDelete}
                 />    
@@ -76,6 +76,9 @@ export default Config;
 class ConfigSection extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            addNew: false
+        }
     }
 
     render() {
@@ -91,30 +94,103 @@ class ConfigSection extends React.Component {
                     <Grid>
                         <Grid.Row>
                             <Grid.Col>
-                                <Heading level="h2">{this.props.name}</Heading>
+                                <Heading level="h2">{this.props.name.charAt(0).toUpperCase()}{this.props.name.slice(1)}</Heading>
                             </Grid.Col>
                             <Grid.Col width="auto">
-            <Button
-                renderIcon={IconPlusLine}
-                margin="small"
-                color="primary"
-            >
-                Add {this.props.name.toLowerCase()} field
-            </Button>
-
+                                <Button
+                                    renderIcon={IconPlusLine}
+                                    margin="small"
+                                    color="primary"
+                                    onClick={() => this.setState({ addNew: !this.state.addNew })}
+                                >
+                                    Add {this.props.name} field
+                                </Button>
                             </Grid.Col>
                         </Grid.Row>
-                    </Grid>
+                        <Grid.Row>
+                            <AddNew 
+                                type={this.props.name}
+                                active={this.state.addNew} />
+                        </Grid.Row>
             {this.props.children && this.props.children.map(c => 
-                <Button
-                    key={c.id}
-                    onClick={() => this.props.onDelete(c.id)}
-                    renderIcon={IconTrashLine}
-                    >
-                    <strong>{c.te_type}</strong> - {c.te_fields.join('.')}
-                </Button>
+                <Grid.Row>
+                    <Button
+                        key={c.id}
+                        onClick={() => this.props.onDelete(c.id)}
+                        renderIcon={IconTrashLine}>
+                        <strong>{c.te_type}</strong> - {c.te_fields.join('.')}
+                    </Button>
+                </Grid.Row>
                 )}
+                    </Grid>
                 </View>
 
     )}
+}
+
+class AddNew extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>
+            {this.props.active && <AddNewForm />}
+            </div>
+        )
+    }
+}
+
+class AddNewForm extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            types: []
+        }
+    }
+    
+    componentDidMount() {
+        let promise = fetch(
+            urlParams(window.injectedEnv.API_URL, "/api/timeedit/types", {})
+        );
+        parseResponse(promise, json => {
+            this.setState({
+                types: Object.entries(json).map(([k, v]) => ({
+                    extid: k,
+                    title: v
+                })),
+                // If the types include "courseevt" we set this as active.
+                // Otherwise just pick the first type.
+                type: Object.keys(json).some(x => x === "courseevt")
+                    ? "courseevt"
+                    : Object.keys(json)[0]
+            });
+        });
+    }
+
+    handleSelect() {
+        console.log("handleSelect()")
+    }
+
+    render() {
+        return (
+                <SimpleSelect
+                    renderLabel="Object Type"
+                    value={this.state.type}
+                    onChange={this.handleSelect("type")}
+                >
+                    {this.state.types.map(t => (
+                        <SimpleSelect.Option
+                            key={t.extid}
+                            id={t.extid}
+                            value={t.extid}
+                        >
+                            {t.title}
+                        </SimpleSelect.Option>
+                    ))}
+                </SimpleSelect>
+        )
+    }
 }
